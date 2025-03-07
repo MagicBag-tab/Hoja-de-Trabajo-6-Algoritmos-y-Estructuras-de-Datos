598518a7c7d67bba2b03af4f9f1bcf4535065080
MagicBag-tab <est24347@uvg.edu.gt>
2025-03-07 12:43:04 -0600

    Estrucutra de la factory

--- Factory.java
+++ Factory.java
@@ -1,3 +1,22 @@
+import java.util.Map;
+
 public class Factory {
 
+    public static Map<String, String> CreateMap(String name) {
+
+        switch (name) {
+            case "HashMap":
+                return;
+
+            case "TreeMap":
+                return;
+
+            case "LinkedHashMap":
+                return;
+
+            default:
+                throw new IllegalArgumentException("Elemento incorreto");
+                break;
+        }
+    }
 }
